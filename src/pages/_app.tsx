345645c898fd314e79feeeb7e34ce01fb859109f
Felipe <felipe.portillo.delgado@gmail.com>
2021-04-26 18:12:29 -0300

    alteracoes

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ChallengesProvider } from '../contexts/ChallengesContexts'
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
+}
+
+export default MyApp
